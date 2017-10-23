1cf65e7f6311cb3e851bc5812fc89486af3f976c
bsiever <bsiever@wustl.edu>
2017-10-23 09:59:52 -0500

    Initial Commit

---
+++ src/studio8/CountSteps.java
@@ -0,0 +1,7 @@
+package studio8;
+
+// TODO: Develop an algorithm to count steps in accelerometer data
+//    Major steeps:
+//       1. Create a class and main method.
+//       2. Using a Scanner and File object, read data from your .csv file.
+//       3. Develop and test algorithms to count the "peaks" in the data.
